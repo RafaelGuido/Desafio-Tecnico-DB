acd973bb7e5fa06fd3c3aea15a63348cf84bdb41
RafaelGuido <rafatocco7@gmail.com>
2022-02-22 12:37:19 -0300

    :memo: :wrench: iniciando configuracao do swagger

---
+++ src/main/java/com/desafio/desafiotecnicodb/core/springfox/SpringFoxConfig.java
@@ -0,0 +1,50 @@
+package com.desafio.desafiotecnicodb.core.springfox;
+
+import com.fasterxml.classmate.TypeResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig implements WebMvcConfigurer {
+
+    @Bean
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.desafio.desafiotecnicodb"))
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo())
+                .tags(new Tag("Receitas","Gerencia as receitas"));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Desafio Tecnico DB")
+                .description("API para documentação do desafio proposto")
+                .version("1")
+                .contact(new Contact("Rafael Guido","https://rafaeltoccolini.com","rafatocco7@gmail.com"))
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");    }
+}
